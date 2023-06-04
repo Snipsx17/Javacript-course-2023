577bbc54148e0e82b3da800d72b2dc3a4d4d2ea0
Snipsx17 <ing.uberthhernandez@gmail.com>
2023-06-04 17:21:34 +0200

    Primer commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,28 @@
+// BMI = mass / (height^2)
+// let massMark = 78;
+// let heightMark = 1.69;
+// let massJohn = 92;
+// let heightJohn = 1.95;
+
+// BMIMark = BMI(massMark, heightMark).toFixed(2);
+// BMIJohn = BMI(massJohn, heightJohn).toFixed(2);
+
+// let markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(`BMI Mark = ${BMIMark}`);
+// console.log(`BMI John = ${BMIJohn}`);
+// console.log(`Mark has a higher BMI than John: ${markHigherBMI}`);
+
+// function BMI(mass, height) {
+//   bmi = mass / height ** 2;
+//   return bmi;
+// }
+
+// TYPE CONVERTION
+let age = "31";
+age = Number(age);
+console.log(typeof age);
+
+// YPE COERCION
+// cuando JS cambia los tipo automaticamente
+console.log("i'm " + age + " years");
